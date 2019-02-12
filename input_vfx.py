914add4c4a756ff490ebcabcf79e7d836eca4c4c
fliu <fangde.liu@outlook.com>
2019-02-13 00:22:56 +0800

    fixed input

--- input_vfx.py
+++ input_vfx.py
@@ -1,4 +1,5 @@
 import numpy as np 
+from scipy import ndimage
 
 def train_input():
     fl=np.load('vfx_seg.npz')
@@ -8,10 +9,13 @@ def train_input():
     print tx.shape
     print ty.shape
 
+    affine=np.array([1,1079/511,1919/511.0,1])
+    affine2=np.array([1,1079/511,1919/511.0])
     
-    
-    tsx=tx[0:128]
-    tsy=ty[0:128].reshape(-1,1080,1920,1)
+    tsx=ndimage.affine_transform(tx[0:64],affine,output_shape=(64,512,512,3))
+    tsy=ndimage.affine_transform(ty[0:64],affine2,output_shape=(64,512,512))
+   
+    tsy=tsy.reshape(-1,512,512,1)
 
 
     del tx
@@ -24,20 +28,13 @@ def train_input():
 
     while True:
 
-        px=np.random.randint(1080-512,size=1)
-        py=np.random.randint(1920-512,size=1)
-        px=px[0]
-        py=py[0]
-
-        print px,py
         
-        tdx=tsx[:,px:px+512,py:py+512,:]
-        tdy=tsy[:,px:px+512,py:py+512,:]
+        
 
-        print tsx.shape,tsy.shape
+        
 
-        tx=(tdx-128.0)/128.0
-        ty=tdy/255.0
+        tx=(tsx-128.0)/128.0
+        ty=tsy/255.0
 
         tx=tx.astype(np.float32)
         ty=ty.astype(np.float32)
